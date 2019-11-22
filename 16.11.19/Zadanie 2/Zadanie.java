29bff7c10dc9f60a5251f1e966d802d6864069f4
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2019-11-22 23:08:49 +0100

    Create Zadanie.java

---
+++ 16.11.19/Zadanie 2/Zadanie.java
@@ -0,0 +1,15 @@
+
+// Napisz własna implementacje klasy Figure (figura) implementujacej interfejs NumberOfSidesPrintable,
+// a nastepnie napisz klase: Square (kwadrat), ktora dziedziczy po Figure oraz implementuje interfejs Resetable.
+// Pokaz w klasie Zadanie uzycie kodu.
+
+
+
+class Zadanie { 
+	
+	public static void main(String[] args) {
+		
+	// podaj przyklad zastosowania
+
+	}
+}
