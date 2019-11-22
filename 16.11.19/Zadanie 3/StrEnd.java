ceaa8a5173de52769ba759b0973aeeb720ff0041
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2019-11-22 23:41:04 +0100

    Update StrEnd.java

--- 16.11.19/Zadanie 3/StrEnd.java
+++ 16.11.19/Zadanie 3/StrEnd.java
@@ -2,10 +2,27 @@
 // Niech program pyta się o parametry dopóki użytkownik nie wpisze tekstu kończącego się literami "end".
 // Spójrz na metody klasy String - aby znaleźć tą która to ułatwi.
 
-public class StrEnd {
-	
-	public static void main(String[] args) {
-		
-	}
+import java.util.*;
+
+public class StrEnd
+{
+
+    public static void main(String[] args)
+    {
+        String wprowadzonyTekst;
+
+        System.out.println();
+        System.out.println("Program zakończy wczytywanie znaków gdy na końcu wpisywanego tekstu pojawi sie 'end'");
+
+        Scanner odczyty = new Scanner(System.in);
+        do
+        {
+            System.out.println("Wprowadz ciąg znaków: ");
+            wprowadzonyTekst = odczyty.nextLine();
+            wprowadzonyTekst = wprowadzonyTekst.toLowerCase();
+        } while (!wprowadzonyTekst.endsWith("end"));
+
+        System.out.println("Koniec");
+    }
 
 }
