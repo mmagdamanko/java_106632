b93dadae8d1a3b2a39314d5ba2023d56830c2e49
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2019-11-22 23:24:41 +0100

    Update Figure.java

--- 16.11.19/Zadanie 2/Figure.java
+++ 16.11.19/Zadanie 2/Figure.java
@@ -6,5 +6,13 @@ public class Figure implements NumberOfSidesPrintable {
 	private String ksztalt; // podział na koło, kwadrat, bryłę itp
 	private int iloscWierzcholkow;
 	private int iloscKatow;
+	
+	public Figure (String rodzaj, String ksztalt, String iloscWierzcholkow, String iloscKatow);
+	{
+		this.rodzaj = rodzaj;
+		this.ksztalt = ksztalt;
+		this.iloscWierzcholkow;
+		this.iloscKatow;
+	}
 
 }
