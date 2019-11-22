db2a10198a08990abc3ea93189ae9d8d5efdf680
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2019-11-22 23:28:46 +0100

    Create StrEnd.java

---
+++ 16.11.19/Zadanie 3/StrEnd.java
@@ -0,0 +1,11 @@
+// Wczytaj z klawiatury kilka parametrów. Wyświetl je na ekran.
+// Niech program pyta się o parametry dopóki użytkownik nie wpisze tekstu kończącego się literami "end".
+// Spójrz na metody klasy String - aby znaleźć tą która to ułatwi.
+
+public class StrEnd {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
