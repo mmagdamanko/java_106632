89168ef9a8c18f0a2238d05d6394d3b048a0e396
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2020-01-06 18:21:36 +0100

    Create Quadruple.java

---
+++ 07.12/Zadanie 2/Quadruple.java
@@ -0,0 +1,9 @@
+public class Quadruple {
+	
+  public static void main(String[] args) {
+	  
+	  // Korzystajšc z klas: Pair oraz Triple (możesz je tu wkleić) - stwórz klasę Quadruple,
+	  //która jest uniwersalnym kontenerem dla 4 typów generycznych. 
+	  // W main pokaż użycie. 
+  }
+}
