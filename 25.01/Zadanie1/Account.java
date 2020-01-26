2dc8de0bafa050b8a44ce832000c947ca10ae4ed
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2020-01-26 14:29:58 +0100

    Create Account.java

---
+++ 25.01/Zadanie1/Account.java
@@ -0,0 +1,38 @@
+// Napisz program w którym zasymulujesz działanie konta bankowego.
+// Niech konto bankowe będzie miało swoje saldo oraz udostępnia metody do pobrania wartości aktualnego salda oraz pobrania z niego pieniędzy.
+
+// Wykonaj następujące czynności:
+// 1. Powołaj do życia kilka wątków, których zadaniem będzie wykonanie następujących akcji:
+
+//	 a) sprawdzenie stanu konta
+//	 b) Poczeka 500ms (Thread.sleep(500))
+//	 c) jeśli stan konta pozwala na pobranie pieniędzy (np. 1000 zł) - pobierze je
+
+// 2. Sprawdź, po zakończeniu pracy wątków, czy stan konta przypadkiem nie jest na minusie?
+// 3. Jeśli tak - spróbuj naprawić sytuację tworząc niektóre metody synchronizacyjnymi
+
+
+public class Account {
+
+	public void Set(double balance){
+		   accountNumber = number();
+		   accountBalance = balance();	
+		}
+	
+	Thread b = new Thread(b);
+	Thread.sleep(500);
+	
+	synchronized int get() {
+		if (resourceEmpty) {
+			Thread.currentThread().getName()
+		}
+	}
+	
+	public static void main(String[] args) {
+		Account a = new Account();
+		a.start();
+	}
+
+	
+	
+}
