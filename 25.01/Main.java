33ae9cdfb205eb5e674a49dbb54d5132e2310180
mmagdamanko <57034138+mmagdamanko@users.noreply.github.com>
2020-01-26 14:22:05 +0100

    Create Main.java

---
+++ 25.01/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<=10;i++) {
+
+            JP watek = new JP();
+            watek.run();
+            try {
+                watek.join();
+            }catch (InterruptedException e){
+                System.out.println("Blad");
+            }
+        }
+
+
+
+
+    }
+}
